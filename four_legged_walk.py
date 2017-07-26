ac0e07efbd6cba36c8d0e1249dbaf66ea5b7d865
Khaled <khaled@robotical.io>
2017-07-26 11:25:08 +0100

    Corrected pin number from 6 to 0

--- four_legged_walk.py
+++ four_legged_walk.py
@@ -12,56 +12,56 @@ def four_legged_walk(head, tail, step_duration = 0.5):
     '''
 
     #Move two alternating legs forward
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, -40), (3, 0), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, -40), (3, 0), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, 0), (3, -40), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
     sleep(step_duration)    #Wait for motion to be excuted
 
     #Move other two legs backward
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, -40), (3, -20), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, -40), (3, -20), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, 20), (3, -40), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
     sleep(step_duration)    #Wait for motion to be excuted
 
     #Straighten first two legs
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, 0), (3, -20), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, 0), (3, -20), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, 20), (3, 0), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
     sleep(step_duration)    #Wait for motion to be excuted
 
     #Straighten all legs
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, 0), (3, 0), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, 0), (3, 0), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, 0), (3, 0), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
     sleep(step_duration)    #Wait for motion to be excuted
 
     #Move two alternating legs forward
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, 0), (3, 40), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, 0), (3, 40), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, -40), (3, 0), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
     sleep(step_duration)    #Wait for motion to be excuted
 
     #Move other two legs backward
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, 20), (3, 40), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, 20), (3, 40), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, -40), (3, 20), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
     sleep(step_duration)    #Wait for motion to be excuted
 
     #Straighten first two legs
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, 20), (3, 0), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, 20), (3, 0), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, 0), (3, 20), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
     sleep(step_duration)    #Wait for motion to be excuted
 
     #Straighten all legs
-    temp_sequence_1 = head.keyframe(step_duration, 4, [(6, 0), (3, 0), (4, 0), (1, 0)])
+    temp_sequence_1 = head.keyframe(step_duration, 4, [(0, 0), (3, 0), (4, 0), (1, 0)])
     head.ros_processed_command(110, *temp_sequence_1)
     temp_sequence_2 = tail.keyframe(step_duration, 4, [(0, 0), (3, 0), (4, 0), (1, 0)])
     tail.ros_processed_command(110, *temp_sequence_2)
